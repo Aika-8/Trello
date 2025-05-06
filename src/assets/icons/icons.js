c753635804e5c8d8c4b980b47ddc7a665f36952d
Aiganysh <dzhumagulova8@gmail.com>
2025-05-06 18:23:00 +0300

    create medias

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,34 @@
+import Arrow from "./arrow.svg?react";
+import Blackboard from "./blackboard.svg?react";
+import Filter from "./filter.svg?react";
+import Image from "./image.svg?react";
+import Kebab from "./kebab.svg?react";
+import Lightning from "./lightning.svg?react";
+import People from "./people.svg?react";
+import Plus from "./plus.svg?react";
+import QuestionMark from "./questionMark.svg?react";
+import Ring from "./ring.svg?react";
+import Rocket from "./rocket.svg?react";
+import Share from "./share.svg?react";
+import Star from "./star.svg?react";
+import SwitcherIcon from "./switcherIcon.svg?react";
+import Table from "./table.svg?react";
+import UseGroup from "./useGroup.svg?react";
+export const Icons = {
+  Arrow,
+  Blackboard,
+  Filter,
+  Image,
+  Kebab,
+  Lightning,
+  People,
+  Plus,
+  QuestionMark,
+  Ring,
+  Rocket,
+  Star,
+  Share,
+  SwitcherIcon,
+  Table,
+  UseGroup,
+};
